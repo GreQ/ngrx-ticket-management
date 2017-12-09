802f16e21874c4c9ff1c15019e73daff2c4e3418
Thomas Burleson <ThomasBurleson@Gmail.com>
2017-12-09 17:55:06 -0600

    Step 1: Biz logic inside View
    Use `.subscribe()` to extract `tickets` data. Manually add to list after `.newTicket()` success.
    
    *  Does not use `async` pipe
    *  Does not handle out-or-rder responses
    
    >  Note that `applyFilters` is similar to a redux selector.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,29 +2,140 @@ import { Component } from '@angular/core';
 import {Ticket} from './models/ticket';
 import {Backend} from './services/backend.service';
 
+/**
+ * Issues:
+ *  1) Does not handle out-or-order
+ *  2) Extracts state instead of using async pipe
+ *  3) Does not use observables
+ *
+ *
+ *  applyFilters() is 'like' a redux selector
+ */
 @Component({
   selector: 'ticket-app',
-  styleUrls: [ './app.component.css' ],
-  template: `      
-      Filter: <input (input)="noop()" #filter>
-      
-      <h2>Tickets</h2>
-      <ul>
-        <li>
-          ({{ticket.id}}) {{ticket.title}}
-          <button (click)="noop()">Complete</button>
-        </li>
-      </ul>
-      
-      <hr> 
-      
-      <input #newTicket>
-      <button (click)="noop()">Add New Ticket</button>
-   `
+  template: `
+    <h2>Tickets</h2>
+    
+    <input (input)="doFilter(assignee.value)" #assignee type="text">
+    
+    <ul>
+      <li *ngFor="let t of tickets">
+        Ticket: {{t.id}}, {{t.title}}
+        <button (click)="doComplete(t)">Complete</button>
+      </li>
+    </ul>
+  
+    <input value="" #newTicket type="text">
+    <button (click)="doAddTicket(newTicket.value)">Add New Ticket</button>
+   `,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  ticket : Ticket = { id:"1", title: "Prepare Challenge Lessons"};
-  constructor(public service: Backend) { }
+  tickets    : Ticket[] = [];   // filtered tickets
+  allTickets : Ticket[] = [];   // all known tickets
+  filter     : string   = "";   // filter criteria
 
-  noop() { }
+  constructor(private backend: Backend) {
+    backend.tickets().subscribe((list:Ticket[]) => {
+      this.allTickets = list;
+      this.tickets    = this.refreshFilteredList();
+    });
+  }
+
+  // ***************************************************************
+  // Actions
+  // ***************************************************************
+
+  doFilter(val:string) {
+    this.filter  = val;
+    this.tickets = this.refreshFilteredList();
+  }
+
+  doAddTicket(title:string) {
+    this.backend
+      .newTicket( {title} )
+      .subscribe((ticket:Ticket) => {
+        this.allTickets = this.addTicket(ticket);
+        this.tickets    = this.refreshFilteredList();
+      });
+  }
+
+  doComplete(ticket:Ticket) {
+    this.backend
+      .complete(ticket.id, true)
+      .subscribe((ticket:Ticket) => {
+        this.allTickets = this.completeTicket(ticket);
+        this.tickets    = this.refreshFilteredList();
+      })
+  }
+
+  // ***************************************************************
+  // 'Action methods'
+  // ***************************************************************
+
+  refreshFilteredList() {
+    return applyFilters(this.filter, this.allTickets);
+  }
+
+  addTicket(ticket:Ticket) {
+    return addTicket(ticket, this.allTickets);
+  }
+
+  completeTicket(ticket:Ticket) {
+    ticket.completed = true;
+    return addTicket( ticket, this.allTickets );
+  }
+
+}
+
+/**
+ * Notice this is analogous to a Redux `reducer` function...
+ * @param list
+ * @param filter
+ */
+function applyFilters(criteria:string, list:Ticket[]):Ticket[] {
+  const inComplete = (t:Ticket):boolean => (t.completed !== true);
+  return list
+    .filter(it => matchesCriteria(criteria,it))
+    .filter( inComplete );
+}
+
+/**
+ * Add ticket to existing list. If already existing,
+ * overwrite with new values. Maintain ticket order.
+ */
+function addTicket(ticket:Ticket, buffer:Ticket[]):Ticket[] {
+  const found = findExisting(ticket, buffer);
+  const result = !!found ? buffer.map(it => {
+      return (it.id == ticket.id) ? {...it, ...ticket} : it;
+  }) : buffer.concat(ticket);
+
+  return result;
+}
+
+
+
+// ***************************************************************
+// Simple data model methods
+// ***************************************************************
+
+/**
+ * Determine if criteria matches ticket description
+ */
+function matchesCriteria(criteria:string, ticket:Ticket):boolean {
+  const description = ticket.description.toLowerCase();
+  const title       = ticket.title.toLowerCase();
+
+    criteria = criteria.toLowerCase();
+
+  return title.includes(criteria) || description.includes(criteria);
+}
+
+/**
+ * Find existing ticket (if present)
+ */
+function findExisting(ticket:Ticket, buffer:Ticket[]):Ticket {
+  return buffer.reduce((prev, curr):Ticket=> {
+    return prev ? prev : ((curr.id == ticket.id) ? curr : null);
+  }, null);
 }
